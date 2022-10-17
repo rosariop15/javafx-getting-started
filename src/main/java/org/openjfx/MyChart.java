a10ef20af71d79bac6c5bd83e7c03cff4cec3346
Rosario Pronsato <rosario.pronsato@ensea.fr>
2022-10-17 11:16:03 +0200

    MyChart Class : creación de clases y muetra de gráficos en fmxl class

---
+++ src/main/java/org/openjfx/MyChart.java
@@ -0,0 +1,33 @@
+package org.openjfx;
+
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
+
+public class MyChart extends VBox {
+    public MyChart () {
+        getChildren().add(buildSampleLineChart());
+    }
+
+    public LineChart buildSampleLineChart() {
+        XYChart.Series<Double , Double> series = new XYChart.Series<>();
+        series.getData().add(new XYChart.Data<>(0.0,0.0));
+        series.getData().add(new XYChart.Data<>(0.7,0.5));
+        series.getData().add(new XYChart.Data<>(1.0,0.632));
+        series.getData().add(new XYChart.Data<>(2.0,0.865));
+        series.getData().add(new XYChart.Data<>(3.0,0.95));
+        series.getData().add(new XYChart.Data<>(4.0,0.982));
+        series.getData().add(new XYChart.Data<>(5.0,0.993));
+
+        LineChart lc = new LineChart(
+                new NumberAxis ("Time Constant ",0.0,5.0,1),
+                new NumberAxis ("Voltage(Vs)",0.0,1.0,0.1)
+        );
+        lc.getData().add(series) ;
+        return lc;
+    }
+
+}
+
+
