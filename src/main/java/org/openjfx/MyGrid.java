5c7c6eefc6cbd93223a238953c7237fca373edef
Rosario Pronsato <rosario.pronsato@ensea.fr>
2022-11-18 15:44:36 +0100

    MyChart Class : creación de clases y muetra de gráficos en fmxl class
    MyHeader : intento de header
    
    button: random numbers in the chart
    
    class Server and Client are now created
    
    now the program is able to send data to another computer given the ip adress

--- src/main/java/org/openjfx/MyGrid.java
+++ src/main/java/org/openjfx/MyGrid.java
@@ -37,6 +37,10 @@ GridPane myGridPane =new GridPane();
         Button button4 = new Button("Button 4");
         Button button5 = new Button("Button 5");
         Button button6 = new Button("Button 6");
+        Button button7 = new Button("Button 7");
+        Button button8 = new Button("Button 8");
+        Button button9 = new Button("Button 9");
+
 
         myGridPane.add(button1, 0, 0, 1, 1);
         myGridPane.add(button2, 1, 0, 1, 1);
@@ -44,6 +48,9 @@ GridPane myGridPane =new GridPane();
         myGridPane.add(button4, 0, 1, 1, 1);
         myGridPane.add(button5, 1, 1, 1, 1);
         myGridPane.add(button6, 2, 1, 1, 1);
+        myGridPane.add(button7, 0, 2, 1, 1);
+        myGridPane.add(button8, 1, 2, 1, 1);
+        myGridPane.add(button9, 2, 2, 1, 1);
 
     }
 
