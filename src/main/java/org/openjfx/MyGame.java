8e0c732e878fa72aa8b3990dc6153123211132f3
Rosario Pronsato <rosario.pronsato@ensea.fr>
2022-11-21 00:42:59 +0100

    Development of game in class MyGame
    Failed attempt to make MyServer work, ii doesn't allow me to edit the PrimaryController, I don't know why

--- src/main/java/org/openjfx/MyGame.java
+++ src/main/java/org/openjfx/MyGame.java
@@ -5,6 +5,90 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class MyGame extends MyGrid {
+    public int winner = -5;
+
+    public int player = 1;
+    public int[][] advance = new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+
+    public void won() {
+        String win;
+        int i;
+        int j;
+        for(i = 0; i < 3; ++i) {
+            win = "True";
+
+            for(j = 0; j < 3; ++j) {
+                if (this.advance[i][j] != this.player) {
+                    win = "false";
+                    break;
+                }
+            }
+
+            if (win == "True") {
+                this.winner = this.player;
+            }
+        }
+
+        for(i = 0; i < 3; ++i) {
+            win = "True";
+
+            for(j = 0; j < 3; ++j) {
+                if (this.advance[j][i] != this.player) {
+                    win = "false";
+                    break;
+                }
+            }
+
+            if (win == "True") {
+                this.winner = this.player;
+            }
+        }
+
+        for(i = 0; i < 3; ++i) {
+            win = "True";
+            if (this.advance[i][i] != this.player) {
+                win = "false";
+                break;
+            }
+
+            if (win == "True") {
+                this.winner = this.player;
+            }
+        }
+
+        for(i = 0; i < 3; ++i) {
+            win = "True";
+            if (this.advance[2 - i][2 - i] != this.player) {
+                win = "false";
+                break;
+            }
+
+            if (win == "True") {
+                this.winner = this.player;
+            }
+        }
+
+    }
+
+    public void isnull() {
+        String win = "tie";
+
+        for(int i = 0; i < 3; ++i) {
+            for(int j = 0; j < 3; ++j) {
+                if (this.advance[i][j] == 0) {
+                    win = "false";
+                    break;
+                }
+            }
+        }
+
+        if (win == "tie") {
+            this.winner = 0;
+        }
+
+    }
+
     @FXML
     MyGame myGame = new MyGame();
 
--- src/main/java/org/openjfx/MyServer.java
+++ src/main/java/org/openjfx/MyServer.java
@@ -4,14 +4,28 @@ import java.net.*;
 import java.io.*;
 
 
-public class MyServer {
+public class MyServer extends Thread {
+
+    //private PrimaryController primaryController;
+    //public MyServer(PrimaryController primaryController) {
+     //   this.primaryController = primaryController;
+    //}
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(6666);
             Socket s = ss.accept(); //establishes connection
             DataInputStream dis = new DataInputStream(s.getInputStream());
+
             String str = (String) dis.readUTF();
             System.out.println("message= " + str);
+
+            Thread updater = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    //primaryController.updateUI(Integer.valueOf(str));
+                }
+            });
+
             ss.close();
         } catch (Exception e) {
             System.err.println(e);
